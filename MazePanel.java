2074cf8a63e7bf08a0b22ca6b15ee034ec148b4a
Raphael Romero <raphael.romero05@aol.com>
2026-08-01 16:05:05 +0800

    feat: maze ui

---
+++ MazePanel.java
@@ -0,0 +1,58 @@
+import javax.swing.*;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class MazePanel extends JPanel {
+    private Maze maze;
+
+    public MazePanel(Maze m) {
+        this.maze = m;
+    }
+
+    public void setMaze(Maze m) {
+        this.maze = m;
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        if (maze != null && maze.isLoaded()) {
+            int rows = maze.getRows();
+            int cols = maze.getCols();
+
+            int cellWidth = this.getWidth() / cols;
+            int cellHeight = this.getHeight() / rows;
+
+            for (int r = 0; r < rows; r++) {
+                for(int c = 0; c < cols; c++) {
+                    Cell cell = maze.getCell(r,c);
+                    char symbol = cell.getSymbol();
+
+                    switch(symbol) {
+                        case '#':
+                            g.setColor(new Color(13,13,104));
+                            break;
+                        case 'S':
+                            g.setColor(Color.YELLOW);
+                            break;
+                        case 'G':
+                            g.setColor(Color.CYAN);
+                            break;
+                        default:
+                            g.setColor(Color.BLACK);
+                            break;
+                    }
+
+                    int x = c * cellWidth;
+                    int y = r * cellHeight;
+                    g.fillRect(x, y, cellWidth, cellHeight);
+
+                    g.setColor(Color.LIGHT_GRAY);
+                    g.drawRect(x,y,cellWidth,cellHeight);
+                }
+            }
+        }
+    }
+}
