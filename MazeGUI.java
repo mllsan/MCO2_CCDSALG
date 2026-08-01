99af3e2194d92906d4b2a8e46a9535e4097e284e
Raphael Romero <raphael.romero05@aol.com>
2026-08-02 01:59:10 +0800

    feat: added metrics, and fixed menu bar configurations

--- MazeGUI.java
+++ MazeGUI.java
@@ -1,12 +1,15 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class MazeGUI {
-    Timer timer;
-    JFrame appFrame;
-    Pathfinder pathfinder;
-    Maze m;
-    MazePanel mazePanel;
+    private Timer timer;
+    private JFrame appFrame;
+    private Pathfinder pathfinder;
+    private Maze m;
+    private MazePanel mazePanel;
+    private JLabel metricsLabel;
+    private JPanel topBar;
 
     public MazeGUI(Maze m, Pathfinder p) {
         this.m = m;
@@ -19,11 +22,14 @@ public class MazeGUI {
         appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         appFrame.setBackground(new Color(13,13,104));
 
-        JPanel topBar = createTopBar();
+        topBar = createTopBar();
         appFrame.add(topBar, BorderLayout.NORTH);
 
         mazePanel = new MazePanel(m);
         appFrame.add(mazePanel, BorderLayout.CENTER);
+
+        JPanel metricBar = metricsBar();
+        appFrame.add(metricBar, BorderLayout.SOUTH);
 
         appFrame.setSize(1000, 700);
         appFrame.setLocationRelativeTo(null);
@@ -37,19 +43,39 @@ public class MazeGUI {
         JButton startButton = new JButton("Start");
         JButton loadButton = new JButton("Load Maze");
         JButton exitButton = new JButton("Exit");
+        JButton menuButton = new JButton("Return to Main Menu");
 
         Font buttonFont = new Font("Monospaced", Font.BOLD, 16);
         Dimension buttonSize = new Dimension(150,45);
+        Border btnBorder = BorderFactory.createLineBorder(Color.WHITE, 2);
 
-        for (JButton b : new JButton[]{startButton, loadButton, exitButton}) {
-            b.setFont(buttonFont);
-            b.setPreferredSize(buttonSize);
-            bar.add(b);
+        if (!pathfinder.isSearching() ) {
+            for (JButton b : new JButton[]{startButton, loadButton, exitButton}) {
+                b.setBackground(Color.black);
+                b.setForeground(Color.WHITE);
+                b.setBorder(btnBorder);
+                b.setFocusPainted(false);
+                b.setFont(buttonFont);
+                b.setPreferredSize(buttonSize);
+                bar.add(b);
+            }
+        }
+        else if (pathfinder.isSearchDone()){
+            for (JButton b : new JButton[]{exitButton, menuButton}) {
+                b.setBackground(Color.black);
+                b.setForeground(Color.WHITE);
+                b.setBorder(btnBorder);
+                b.setFocusPainted(false);
+                b.setFont(buttonFont);
+                b.setPreferredSize(buttonSize);
+                bar.add(b);
+            }
         }
 
         startButton.addActionListener(e -> animate());
         loadButton.addActionListener(e -> chooseMazes());
         exitButton.addActionListener(e -> System.exit(0));
+        menuButton.addActionListener(e -> menu());
 
         return bar;
     }
@@ -97,17 +123,33 @@ public class MazeGUI {
 
             pathfinder.init();
             mazePanel.repaint();
+            topBar = createTopBar();
+            topBar.repaint();
 
-            timer = new Timer(50, e -> {
+            timer = new Timer(75, e -> {
                 pathfinder.move();
                 mazePanel.repaint();
 
+                metricsLabel.setText("Cells Visited: " + pathfinder.getCellsVisited() +
+                        " | Path Length: " + pathfinder.getPathLength() +
+                        " | Status: Searching for path...");
+
                 if(pathfinder.isSearchDone()) {
                     ((Timer) e.getSource()).stop();
-                    displayMetrics();
 
-                    if(pathfinder.isGoalFound()) {
-                        JOptionPane.showMessageDialog(appFrame, "There is no valid path found!");
+                    if(!pathfinder.isGoalFound()) {
+                        metricsLabel.setText("Cells Visited: " + pathfinder.getCellsVisited() +
+                                " | Path Length: " + pathfinder.getPathLength() +
+                                " | Status: No path was found! | Execution Time: "
+                                + pathfinder.getExecutionTimeMillis() + " ms");
+                        pathfinder.resetTimers();
+                    }
+                    else {
+                        metricsLabel.setText("Cells Visited: " + pathfinder.getCellsVisited() +
+                                " | Path Length: " + pathfinder.getPathLength() +
+                                " | Status: Goal found! | Execution Time: "
+                                + pathfinder.getExecutionTimeMillis() + " ms");
+                        pathfinder.resetTimers();
                     }
                 }
             });
@@ -118,16 +160,15 @@ public class MazeGUI {
         }
     }
 
-    public void displayMetrics() {
+    public JPanel metricsBar() {
+        JPanel metricsBar = new JPanel(new FlowLayout(FlowLayout.CENTER, 20,10));
+        metricsBar.setBackground(Color.black);
 
-    }
+        metricsLabel = new JLabel("Cells Visited: 0 | Path Length: 0 | Status: Not Started");
+        metricsLabel.setForeground(Color.white);
+        metricsLabel.setFont(new Font("Monospaced", Font.BOLD, 14));
+        metricsBar.add(metricsLabel);
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            Maze m = new Maze();
-            PriorityQueue pq = new PriorityQueue();
-            Pathfinder p = new Pathfinder(m, pq);
-            new MazeGUI(m,p).menu();
-        });
+        return metricsBar;
     }
 }
